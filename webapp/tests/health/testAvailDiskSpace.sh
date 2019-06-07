c50f80f7cb29095cee4ded5093170c71289bc36c
Michael Farb <mwfarb@andrew.cmu.edu>
2019-06-07 17:19:33 -0400

    fix GB vs GiB in memeory tests

--- webapp/tests/health/testAvailDiskSpace.sh
+++ webapp/tests/health/testAvailDiskSpace.sh
@@ -1,4 +1,4 @@
-# test if the available disk space is greater than 2G, fail if not
+# test if the available disk space is greater than 2GB, fail if not
 
 # An error exit function
 error_exit()
@@ -9,13 +9,12 @@ error_exit()
 
 # get the available space for this virtual machine
 availSpace=$(df | grep '/' -w | tr -s ' ' | cut -d ' ' -f4)
-echo "Size of available space: $((availSpace / 1024000))G."
+echo "Size of available space: $((availSpace / 1000000))GB."
 
-# test if the available disk space is greater than 2G
-if [ "$availSpace" -lt 2048004 ]; then
-    error_exit "Error: Available disk space less than 2G, please destroy your virtual machine and create a new one"
+# test if the available disk space is greater than 2GB (not GiB)
+if [ "$availSpace" -lt 2000000 ]; then
+    error_exit "Error: Available disk space less than 2GB, please destroy your virtual machine and create a new one"
 else
     echo "Test for available disk space succeeds."
     exit 0
 fi
-
--- webapp/tests/health/testTotalMem.sh
+++ webapp/tests/health/testTotalMem.sh
@@ -1,4 +1,4 @@
-# test if the total memory is greater than 2G, fail if not
+# test if the total memory is greater than 2GB, fail if not
 
 # An error exit function
 error_exit()
@@ -8,15 +8,13 @@ error_exit()
 }
 
 # get the total memory for this virtual machine
-totalMem=$(free | grep 'Mem:' | tr -s ' ' | cut -d ' ' -f2)
-echo "Size of total memory: $((totalMem / 1024000))G."
+totalMem=$(free --si | grep 'Mem:' | tr -s ' ' | cut -d ' ' -f2)
+echo "Size of total memory: $((totalMem / 1000000))GB."
 
-# test if the total memory is greater than 2G
-if [ "$totalMem" -lt 2048004 ]; then
-     error_exit "Error: Total memory less than 2G, please contact us."
+# test if the total memory is greater than 2GB (not GiB)
+if [ "$totalMem" -lt 2000000 ]; then
+     error_exit "Error: Total memory less than 2GB, please contact us."
 else
     echo "Test for total memory succeeds."
     exit 0
 fi
-
-
