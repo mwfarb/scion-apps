3bb1fea2eefe0fed16426c4fa0bbf0de81bbef38
Michael Farb <mwfarb@andrew.cmu.edu>
2019-10-18 09:25:35 -0400

    move js to static file and reformat

---
+++ webapp/web/static/js/video.js
@@ -0,0 +1,303 @@
+// Copyright 2019 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.package main
+
+window.onload = function(event) {
+    $("#videoout").empty();
+    debugLog("Loaded page");
+    yourVideo = $("#yourVideo");
+    friendsVideo = $("#friendsVideo");
+    showMyIa(yourIa);
+    ajaxConfig();
+};
+
+function debugLog(msg) {
+    console.log(msg);
+    $("#videoout").append("\n" + msg + ".");
+    $(".stdout").scrollTop($(".stdout")[0].scrollHeight);
+}
+
+function ajaxConfig() {
+    return $.ajax({
+        url : 'config',
+        type : 'get',
+        dataType : "json",
+        timeout : 30000,
+        success : isRTCConfigComplete,
+        error : function(jqXHR, textStatus, errorThrown) {
+            debugLog(this.url + ' ' + textStatus + ': ' + errorThrown);
+        },
+    });
+}
+
+// App's Firebase configuration
+var firebaseConfig = {
+    authDomain : "wip-scion-webrtc.firebaseapp.com",
+    databaseURL : "https://wip-scion-webrtc.firebaseio.com",
+    projectId : "wip-scion-webrtc",
+    storageBucket : "wip-scion-webrtc.appspot.com",
+};
+
+var servers = {
+    'iceServers' : [ {
+        'urls' : 'stun:stun.services.mozilla.com'
+    }, {
+        'urls' : 'stun:stun.l.google.com:19302'
+    } ]
+};
+var pc = new RTCPeerConnection(servers);
+var database;
+var yourVideo;
+var friendsVideo;
+
+function isRTCConfigComplete(data, textStatus, jqXHR) {
+    debugLog(this.url + ' ' + textStatus);
+    debugLog('firebaseConfig.apiKey = ' + data.webrtc_apiKey);
+    debugLog('firebaseConfig.messagingSenderId = '
+            + data.webrtc_messagingSenderId);
+    debugLog('firebaseConfig.appId = ' + data.webrtc_appId);
+
+    firebaseConfig.apiKey = data.webrtc_apiKey;
+    firebaseConfig.messagingSenderId = data.webrtc_messagingSenderId;
+    firebaseConfig.appId = data.webrtc_appId;
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    database = firebase.database().ref();
+
+    pc.onicecandidate = function(event) {
+        event.candidate ? sendMessage(yourId, JSON.stringify({
+            'ice' : event.candidate
+        })) : console.log("Sent All Ice");
+    };
+    pc.onaddstream = function(event) {
+        setupSigConnection();
+        friendsVideo.srcObject = event.stream;
+        // $("#hangup-button").disabled = false;
+    };
+    database.on('child_added', readMessage);
+
+    // when load finished...
+    showMyFace();
+}
+
+function sendMessage(senderId, data) {
+    debugLog("Called sendMessage()");
+    var msg = database.push({
+        sender : senderId,
+        ia : myIa,
+        message : data
+    });
+    msg.remove();
+    updateAddrs();
+}
+
+function readMessage(data) {
+    debugLog("Called readMessage()");
+    var msg = JSON.parse(data.val().message);
+    var sender = data.val().sender;
+    if (sender != yourId) {
+        friendsIa = data.val().ia;
+        showPeerIa(friendsIa);
+        if (msg.ice != undefined) {
+            pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+        } else if (msg.sdp.type == "offer") {
+            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+            //
+            .then(function() {
+                pc.createAnswer();
+            })
+            //
+            .then(function(answer) {
+                pc.setLocalDescription(answer);
+            })
+            //
+            .then(function() {
+                sendMessage(yourId, JSON.stringify({
+                    'sdp' : pc.localDescription
+                }));
+            });
+        } else if (msg.sdp.type == "answer") {
+            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+        }
+    }
+    updateAddrs();
+};
+
+function setupSigConnection() {
+    if (!pc.currentLocalDescription) {
+        debugLog();
+    }
+    var form_data = [ {
+        "name" : "ia_cli",
+        "value" : yourIa
+    }, {
+        "name" : "addr_cli",
+        "value" : getSdpAddr(pc.currentLocalDescription.sdp)
+    }, {
+        "name" : "port_cli",
+        "value" : "30001"
+    }, {
+        "name" : "ia_ser",
+        "value" : friendsIa
+    }, {
+        "name" : "addr_ser",
+        "value" : getSdpAddr(pc.currentRemoteDescription.sdp)
+    }, {
+        "name" : "port_ser",
+        "value" : "30100"
+    }, {
+        "name" : "apps",
+        "value" : "sig"
+    }, {
+        "name" : "continuous",
+        "value" : false
+    }, {
+        "name" : "interval",
+        "value" : "1"
+    } ];
+    debugLog('req: ' + JSON.stringify(form_data));
+    $.post('/command', form_data, function(resp, status, jqXHR) {
+        debugLog('resp: ' + resp);
+        // handleSigResponse(resp);
+    }).fail(function(error) {
+        debugLog('error: ' + error.responseJSON);
+        // handleGeneralResponse();
+    });
+}
+
+function updateAddrs() {
+    if (pc.currentLocalDescription) {
+        debugLog("yourType: " + pc.currentLocalDescription.type);
+        showMyAddr(getSdpAddr(pc.currentLocalDescription.sdp));
+        showMyAudio(getSdpAudio(pc.currentLocalDescription.sdp));
+        showMyVideo(getSdpVideo(pc.currentLocalDescription.sdp));
+    }
+    if (pc.currentRemoteDescription) {
+        debugLog("friendsType: " + pc.currentRemoteDescription.type);
+        showPeerAddr(getSdpAddr(pc.currentRemoteDescription.sdp));
+        showPeerAudio(getSdpAudio(pc.currentRemoteDescription.sdp));
+        showPeerVideo(getSdpVideo(pc.currentRemoteDescription.sdp));
+    }
+}
+
+function showMyFace() {
+    debugLog("Called showMyFace()");
+    navigator.mediaDevices.getUserMedia({
+        audio : true,
+        video : true
+    })
+    //
+    .then(function(stream) {
+        yourVideo.srcObject = stream;
+    })
+    //
+    .then(function(stream) {
+        pc.addStream(stream);
+    });
+}
+
+function showFriendsFace() {
+    debugLog("Called showFriendsFace()");
+    pc.createOffer()
+    //
+    .then(function(offer) {
+        pc.setLocalDescription(offer);
+    })
+    //
+    .then(function() {
+        sendMessage(yourId, JSON.stringify({
+            'sdp' : pc.localDescription
+        }));
+    });
+}
+
+function showMyIa(ia) {
+    debugLog("yourIaText: " + ia);
+    $("#yourIaText").html(ia);
+}
+
+function showPeerIa(ia) {
+    debugLog("friendsIaText: " + ia);
+    $("#friendsIaText").html(ia);
+}
+
+function showMyAddr(addr) {
+    debugLog("yourVar1: " + addr);
+    $("#yourVar1").html(addr);
+}
+
+function showPeerAddr(addr) {
+    debugLog("friendsVar1: " + addr);
+    $("#friendsVar1").html(addr);
+}
+
+function showMyAudio(addr) {
+    debugLog("yourVar2: " + addr);
+    $("#yourVar2").html(addr);
+}
+
+function showPeerAudio(addr) {
+    debugLog("friendsVar2: " + addr);
+    $("#friendsVar2").html(addr);
+}
+
+function showMyVideo(addr) {
+    debugLog("yourVar3: " + addr);
+    $("#yourVar3").html(addr);
+}
+
+function showPeerVideo(addr) {
+    debugLog("friendsVar3: " + addr);
+    $("#friendsVar3").html(addr);
+}
+
+function getSdpAddr(sdp) {
+    var ips = [];
+    // console.error(sdp)
+    if (sdp) {
+        ips = sdp
+                .split('\r\n')
+                .filter(function(line) {
+                    return line.indexOf('c=') === 0;
+                })
+                .map(
+                        function(ipstr) {
+                            return ipstr
+                                    .match(/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/)[0];
+                        });
+    }
+    return ips[0];
+}
+
+function getSdpAudio(sdp) {
+    var ips = [];
+    // console.error(sdp)
+    if (sdp) {
+        ips = sdp.split('\r\n').filter(function(line) {
+            return line.indexOf('m=audio') === 0;
+        });
+    }
+    return ips[0];
+}
+
+function getSdpVideo(sdp) {
+    var ips = [];
+    // console.error(sdp)
+    if (sdp) {
+        ips = sdp.split('\r\n').filter(function(line) {
+            return line.indexOf('m=video') === 0;
+        });
+    }
+    return ips[0];
+}
